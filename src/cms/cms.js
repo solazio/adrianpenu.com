e84a0e0b4a21ce873c49334257c363fe18341930
solazio <ovidiu.suciu@canonical.com>
2020-07-17 17:05:07 +0100

    Update cms preview template name

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,16 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+import CMS from "netlify-cms-app";
+import uploadcare from "netlify-cms-media-library-uploadcare";
+import cloudinary from "netlify-cms-media-library-cloudinary";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
-import CategoryPagePreview from './preview-templates/CategoryPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import CategoryPagePreview from "./preview-templates/CategoryPagePreview";
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate("category", CategoryPagePreview);
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
+CMS.registerPreviewTemplate("paintings", CategoryPagePreview);
